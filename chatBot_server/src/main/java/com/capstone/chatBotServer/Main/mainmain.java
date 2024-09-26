ad9f69a9e026e59448acf0f195f14df1ca50a29e
hhjkhhbgkugjnbj1 <ck8901ck@gmail.com>
2024-09-26 18:11:34 +0900

    gemini ai chatbot spring server

--- chatBot_server/src/main/java/com/capstone/chatBotServer/Main/mainmain.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.chatBotServer.Main;
-
-public class mainmain {
-	public int mainmain() {
-		return 1;
-	}
-}
--- chatBot_server/src/main/java/com/capstone/chatBotServer/Service/chatBotservice.java
+++
@@ -1,6 +0,0 @@
-package com.capstone.chatBotServer.Service;
-
-public interface chatBotservice {
-
-	public String sendToPythonMessage(String message) throws Exception;
-}
